2bc069271cd681621c8baad95eca59856dfccbc2
durw4rd <misa.fasa@gmail.com>
2016-04-15 11:43:17 +0200

    delete items button

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -40,25 +40,21 @@ class NewsItemsController < ApplicationController
   # PATCH/PUT /news_items/1
   # PATCH/PUT /news_items/1.json
   def update
-    respond_to do |format|
-      if @news_item.update(news_item_params)
-        format.html { redirect_to @news_item, notice: 'News item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @news_item }
-      else
-        format.html { render :edit }
-        format.json { render json: @news_item.errors, status: :unprocessable_entity }
-      end
-    end
+    @news_item = NewsItem.find( params[:id] )
+
+    @news_item.destroy
+
+    redirect_to anews_items_path
   end
 
   # DELETE /news_items/1
   # DELETE /news_items/1.json
   def destroy
+    @news_item = NewsItem.find( params[:id] )
+
     @news_item.destroy
-    respond_to do |format|
-      format.html { redirect_to news_items_url, notice: 'News item was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+
+    redirect_to news_items_path
   end
 
   private
